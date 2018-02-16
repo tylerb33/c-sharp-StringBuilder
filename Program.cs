9026e89ab721ce2954432feaa48bc3a0693e7791
Tyler Barnett <tyler.barnett7190@gmail.com>
2018-02-16 11:21:47 -0600

    basic example finished

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            while (true)
+            {
+                Console.WriteLine("Type a grocery list item:");
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+                else
+                {
+                    builder.Append(input + " ");
+                }
+            }
+                
+            Console.WriteLine("Be sure to buy {0}", builder.ToString());
+            Console.ReadKey();
+        }
+    }
+}
